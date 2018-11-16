a622d9875b45437c7bc1f0ad5d1d21b15b730063
chihkuo <mooijkuo@gmail.com>
2018-11-16 17:01:20 +0800

    modify make package script

--- code/MakePackage/MakePackage.sh
+++ code/MakePackage/MakePackage.sh
@@ -1,19 +1,51 @@
-mkdir update
+function copy() {
+	echo "run function copy()"
+	if [ ! -d "update" ]; then
+		mkdir update
+	fi
+	cp ../datalog-h5000/dlg320.exe ./update/
+	cp ../DataProgram/DataProgram.exe ./update/
+	cp ../script/dldevice.lua ./update/
+	cp ../script/dllist ./update/
+	cp ../script/dllist.lua ./update/
+	cp ../script/dlsetting ./update/
+	cp ../script/dlsetting.lua ./update/
+	cp ../script/ModelList ./update/
+	cp ../script/system.lua ./update/
+	cp ../script/update.sh ./update/
+	sync
+}
 
-cp ../datalog-h5000/dlg320.exe ./update/
-cp ../DataProgram/DataProgram.exe ./update/
-cp ../script/dldevice.lua ./update/
-cp ../script/dllist ./update/
-cp ../script/dllist.lua ./update/
-cp ../script/dlsetting ./update/
-cp ../script/dlsetting.lua ./update/
-cp ../script/ModelList ./update/
-cp ../script/system.lua ./update/
-cp ../script/update.sh ./update/
+function package() {
+	echo "run function package()"
+	tar -cvf update_$1.tar update
+	sync
+}
 
-tar -cvf update_$1.tar update
+function clean() {
+	echo "run function clean()"
+	rm -rf update
+}
 
-sync
+function all() {
+	echo "run function all()"
+	copy;
+	package $1;
+	clean;
+}
 
-rm -rf update
+if [ $1 == "copy" ]; then
+	copy;
+fi
 
+if [ $1 == "package" ]; then
+	package $2;
+fi
+
+if [ $1 == "clean" ]; then
+	clean;
+fi
+
+if [ $1 == "all" ]; then
+	all $2;
+fi
